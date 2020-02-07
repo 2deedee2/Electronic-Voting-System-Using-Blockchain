83e1b1545ac6edd5aae55dc499e990ef5db019b7
2deedee2 <ivjoe777@hotmail.com>
2020-02-07 18:25:38 +0000

    Init CSS Style

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,10 +10,10 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="/">Home</a>
+        <a class="navbar-brand" href="/">Electronic Voting System Using Blockchain</a>
         <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/login">
-          <li class="nav-item" ><a class="nav-link" href="#">
+          <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
+          <li class="nav-item" ><a class="nav-link" href="/register">Register</a></li>
         </ul>
       </div>
 </nav>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,12 @@
 @section('title',"Electronic Blockchain Voting")
 
 @section('content')
-    <h1>Home Page Test</h1>
+<div class="jumbotron">
+    <h1 class="display-7">2020 Student Union Election</h1>
+    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum provident omnis hic placeat ad dolore quibusdam minima ipsum rem eos? Esse ducimus unde tempora cupiditate ea reiciendis consequatur recusandae facere.</p>
+    <hr class="my-4">
+
+     <a class="btn btn-primary btn-lg" href="election.html" role="button">Start Voting</a>
+</div>
+
 @stop
