72f57b5b26e59d5509be04193fef2da190acd3fd
2deedee2 <ivjoe777@hotmail.com>
2020-02-07 18:31:45 +0000

    Solved Broswer Cache

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title','Blockchain Voting')</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
