3573b0575f7ef0101579f09c19e01c29cc6fc738
2deedee2 <ivjoe777@hotmail.com>
2020-02-08 15:52:25 +0000

    Register form

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,6 +1,37 @@
 @extends('layouts.default')
 @section('title',"Register")
 @section('content')
-<h1>Login PAGE TEST</h1>
 
+<div class="offset-md-2 col-md-8">
+    <div class="card ">
+        <div class="card-header">
+            <h5>Register</h5>
+        </div>
+        <div class="card-body">
+            <form method="POST" action="{{ route('users.store') }}">
+                <div class="form-group">
+                    <label for="name">Name: </label>
+                    <input type="text" name="name" class="form-control" value="{{ old('name')}}">
+                </div>
+
+            <div class="form-group">
+                <label for="email">Email: </label>
+                <input type="text" name="email" class="form-control" value="{{ old('email')}}">
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password: </label>
+                <input type="password" name="password" class="form-control" value="{{ old('password')}}">
+            </div>
+
+            <div class="form-group">
+                <label for="password_confirmation">Password Confrimation</label>
+                <input type="password" name="password_confirmation" class="form-control" value="{{old('password_confirmation')}}">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
+</div>
 @stop
