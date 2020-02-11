2191744c52ffe3e5735830a52c0bd0d8aeddeb9f
2deedee2 <ivjoe777@hotmail.com>
2020-02-11 12:45:10 +0000

    finished 6 cards vote

--- app/Http/Controllers/ElectionController.php
+++ app/Http/Controllers/ElectionController.php
@@ -14,16 +14,36 @@ class ElectionController extends Controller
 
     public function store()
     {
-       if  (isset($_POST['1'])){
-        $result = $this->PostDataToChain('Joe','1');
-        //$result = $this->getChain();
-        return $result;
-        //session()->flash('success','Your ballot is added into blockchain.');
-        //return redirect()->route('home');
+       if(isset($_POST['1'])){
+        $blockNo = $this->PostDataToChain('Joe','1');
+        session()->flash('success',"Thanks! Your ballot has been added into blockchain. (Block $blockNo)");
+        return redirect()->route('home');
        }
-       else{
-           return "hellpo";
+       else if(isset($_POST['2'])){
+        $blockNo = $this->PostDataToChain('Joe','2');
+        session()->flash('success',"Thanks! Your ballot has been added into blockchain. (Block $blockNo)");
+        return redirect()->route('home');
        }
+       else if(isset($_POST['3'])){
+        $blockNo = $this->PostDataToChain('Joe','3');
+        session()->flash('success',"Thanks! Your ballot has been added into blockchain. (Block $blockNo)");
+        return redirect()->route('home');
+        }
+        else if(isset($_POST['4'])){
+            $blockNo = $this->PostDataToChain('Joe','4');
+            session()->flash('success',"Thanks! Your ballot has been added into blockchain. (Block $blockNo)");
+            return redirect()->route('home');
+        }
+        else if(isset($_POST['5'])){
+            $blockNo = $this->PostDataToChain('Joe','5');
+            session()->flash('success',"Thanks! Your ballot has been added into blockchain. (Block $blockNo)");
+            return redirect()->route('home');
+        }
+        else if(isset($_POST['6'])){
+            $blockNo = $this->PostDataToChain('Joe','6');
+            session()->flash('success',"Thanks! Your ballot has been added into blockchain. (Block $blockNo)");
+            return redirect()->route('home');
+        }
 
     }
 
@@ -41,10 +61,13 @@ class ElectionController extends Controller
         curl_setopt($ch , CURLOPT_POST , 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         //echo "<h1>Thanks for your participation!</h1>";
         //echo $jsonDataEncoded . "</br>";
-        $result = curl_exec($ch);
-        return $jsonDataEncoded;
+        $respone = curl_exec($ch);
+        $str = json_decode($respone);
+        $blockNo = $str->indexNo;
+        return $blockNo;
     }
 
     public function getChain()
